6d6bf672748937286197576565cc3ca4864879a1
Art Macias <art.maciasjr@gmail.com>
2018-10-29 18:36:17 -0600

    Added Style Changes

--- app/src/main/java/com/example/shawnli/pickupball/popUpDialog.java
+++ app/src/main/java/com/example/shawnli/pickupball/popUpDialog.java
@@ -7,6 +7,7 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.example.shawnli.pickupball.Model.Court;
 
@@ -18,19 +19,27 @@ public class popUpDialog extends DialogFragment {
         // Use the Builder class for convenient dialog construction
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         builder.setMessage(R.string.added_to_game)
-                .setPositiveButton(R.string.take_me, new DialogInterface.OnClickListener() {
+                .setPositiveButton(R.string.done, new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
-                        Uri gmmIntentUri = Uri.parse("google.navigation:q="+currentCourt.getAddress());
-
-                        // Create an Intent from gmmIntentUri. Set the action to ACTION_VIEW
-                        Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
-                        // Make the Intent explicit by setting the Google Maps package
-                        mapIntent.setPackage("com.google.android.apps.maps");
-
-                        // Attempt to start an activity that can handle the Intent
-                        startActivity(mapIntent);
+                        // Do nothing and return to activity.
                     }
-                });
+                })
+                .setNegativeButton(R.string.take_me,
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int whichButton) {
+                                Uri gmmIntentUri = Uri.parse("google.navigation:q="+currentCourt.getAddress());
+
+                                // Create an Intent from gmmIntentUri. Set the action to ACTION_VIEW
+                                Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+                                // Make the Intent explicit by setting the Google Maps package
+                                mapIntent.setPackage("com.google.android.apps.maps");
+
+                                // Attempt to start an activity that can handle the Intent
+                                startActivity(mapIntent);
+
+                            }
+                        }
+                );
         // Create the AlertDialog object and return it
         return builder.create();
 
